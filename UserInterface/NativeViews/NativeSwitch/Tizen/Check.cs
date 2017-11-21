71020a801c3cdb96ce08cf524a7134c3f38e51ca
Joonghyun Cho <jh5.cho@samsung.com>
2017-11-21 15:38:57 +0900

    Add Tizen project for NativeViews/NativeSwitch

---
+++ UserInterface/NativeViews/NativeSwitch/Tizen/Check.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms.Platform.Tizen;
+
+namespace NativeSwitch.Tizen
+{
+    class Check : ElmSharp.Check
+    {
+        public Check() : base(Forms.Context.MainWindow)
+        {
+        }
+    }
+}
