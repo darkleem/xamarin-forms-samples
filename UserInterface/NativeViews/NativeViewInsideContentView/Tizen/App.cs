50078e034e2d9530bd349707ddf265584c9ac718
Joonghyun Cho <jh5.cho@samsung.com>
2017-11-23 15:08:10 +0900

    Add Tizen project for NativeViews/NativeViewInsideContentView

---
+++ UserInterface/NativeViews/NativeViewInsideContentView/Tizen/App.cs
@@ -0,0 +1,21 @@
+﻿namespace NativeViewInsideContentView.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        static public ElmSharp.Window NativeMainWindow => Xamarin.Forms.Platform.Tizen.Forms.Context.MainWindow;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+
+}
