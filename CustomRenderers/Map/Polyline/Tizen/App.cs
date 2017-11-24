3c8473026261b6bb41817ef05bb454b45303a993
Chungryeol lim <darkleem@naver.com>
2017-11-24 15:07:19 +0900

    Add Tizen project for CustomRenderers/Map/Polyline

---
+++ CustomRenderers/Map/Polyline/Tizen/App.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MapOverlay.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            global::Xamarin.FormsMaps.Init("HERE", "pE-W9LeqN7zB9RtnwgBN/tZuCgj-LtWQ4RWN56XrVpA");
+            app.Run(args);
+        }
+    }
+}
